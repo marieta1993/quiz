610c2b820741e2dd5150ec018cb19ff8894bd6e6
Marieta Avetisyan <avetisyanmarieta1993@gmail.com>
2024-04-11 18:08:07 +0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/marieta/com/quiz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.marieta.com.quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/quiz_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  State<QuizScreen> createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  int incrementValue = 0;
+  int decrementValue = 0;
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: InkWell(
+                    onTap: () {
+                      onContainerPressed(true);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            incrementValue.toString(),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      decrementValue = 0;
+                      incrementValue = 0;
+                      counter = 0;
+                    });
+                  },
+                  child: const Text('Zero')),
+              Expanded(
+                child: InkWell(
+                    onTap: () {
+                      onContainerPressed(false);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            decrementValue.toString(),
+                          ),
+                        ],
+                      ),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  onContainerPressed(bool isIncrement) {
+    if (isIncrement) {
+      counter++;
+      setState(() {
+        incrementValue = counter;
+      });
+    } else {
+      counter--;
+      setState(() {
+        decrementValue = counter;
+      });
+    }
+  }
+}
